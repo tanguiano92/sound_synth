6316a7076cfec6de900dd3ff80bf6ed91418a251
tanguiano92 <47006589+tanguiano92@users.noreply.github.com>
2019-11-01 15:23:11 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,23 @@
+let rainSound;
+let makeRain;
+
+function preload(){
+  soundFormats('mp3', 'ogg');
+  rainSound = loadSound('audio/rain.mp3');
+}
+
 function setup() {
   noCanvas();
+
+  makeRain = select('#makeRainSound');
+  makeRain.mousePressed(rain);
 }
 
 function draw() {
   background(220);
+}
+
+function rain(){
+  rainSound.setVolume(0.1);
+  rainSound.play();
 }
