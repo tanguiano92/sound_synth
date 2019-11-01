7521c96bde91c51275c726826cbf0c1769c055f7
tanguiano92 <47006589+tanguiano92@users.noreply.github.com>
2019-11-01 16:07:24 -0700

    sound

--- sketch.js
+++ sketch.js
@@ -1,5 +1,10 @@
 let rainSound;
 let makeRain;
+let button;
+
+var wave;
+
+let playing;
 
 function preload(){
   soundFormats('mp3', 'ogg');
@@ -11,13 +16,33 @@ function setup() {
 
   makeRain = select('#makeRainSound');
   makeRain.mousePressed(rain);
+
+  wave = new p5.Oscillator();
+  wave.setType('sine');
+  wave.start();
+  wave.amp(0);
+    wave.freq(1000);
+
+  button = createButton('play.pause')
+  button.mousePressed(toggle);
 }
 
 function draw() {
-  background(220);
+
 }
 
 function rain(){
   rainSound.setVolume(0.1);
   rainSound.play();
+}
+
+function toggle(){
+if (!playing) {
+      wave.amp(0.5, 1);
+  playing = true;
+} else {
+  wave.stop();
+  wave.amp(0, 1);
+  playing = false;
+}
 }
