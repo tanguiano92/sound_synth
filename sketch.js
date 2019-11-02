d3bc8c625b8d636eb402ed9d15a6a66a89cdcbbb
tanguiano92 <47006589+tanguiano92@users.noreply.github.com>
2019-11-01 18:36:48 -0700

    update

--- sketch.js
+++ sketch.js
@@ -1,6 +1,14 @@
 let rainSound;
 let makeRain;
 let button;
+let bees;
+let makeBee;
+let peeps;
+let springPeepers;
+let stormy;
+let storm;
+let spill;
+let water;
 
 var wave;
 
@@ -12,6 +20,10 @@ var env;
 function preload(){
   soundFormats('mp3', 'ogg');
   rainSound = loadSound('audio/rain.mp3');
+  makeBee = loadSound('audio/bees.mp3');
+  springPeepers = loadSound('audio/spring_peepers.mp3');
+  storm = loadSound('audio/thunderstorm.mp3')
+  water = loadSound('audio/water_in_glass_spill.mp3');
 }
 
 function setup() {
@@ -23,6 +35,15 @@ env.setRange(1.2, 0);
 
   makeRain = select('#makeRainSound');
   makeRain.mousePressed(rain);
+  spill = select('#makeWaterSpill');
+  spill.mousePressed(waters);
+  bees = select('#makeBees');
+  bees.mousePressed(bee);
+  stormy = select('#thunder');
+  stormy.mousePressed(storms);
+
+  peeps = select('#makeSpringPeepers');
+  peeps.mousePressed(springPeeps);
 
   wave = new p5.Oscillator();
   wave.setType('sine');
@@ -31,7 +52,7 @@ env.setRange(1.2, 0);
   wave.amp(env);
 
 
-  button = createButton('play.pause')
+  button = createButton('play,pause')
   button.mousePressed(toggle);
 
   slider = createSlider(100, 1200, 440);
@@ -44,6 +65,22 @@ wave.freq(slider.value());
 function rain(){
   rainSound.setVolume(0.1);
   rainSound.play();
+}
+function bee(){
+  makeBee.setVolume(0.1);
+  makeBee.play();
+}
+function springPeeps(){
+  springPeepers.setVolume(0.1);
+  springPeepers.play();
+}
+function storms(){
+  storm.setVolume(0.1);
+  storm.play();
+}
+function waters(){
+  water.setVolume(0.2);
+  water.play();
 }
 
 function toggle(){
